1d093b73c326954dc6a33d8fb78a2bafa0c435e2
Tiara S Dewi <trsrdw@gmail.com>
2025-10-19 21:46:07 +0700

    Remove unnecessary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,38 +5,6 @@ import JourneySection from "@/components/Page/Home/Journey/journey";
 import ProjectsSection from "@/components/Page/Home/Projects/projects";
 import ProfileSection from "@/components/Page/Home/Profile/profile";
 
-let metadataObj;
-if (process.env.NEXT_PUBLIC_ENVIRONMENT === "production") {
-    metadataObj = {
-        title: "Rick and Morty Multiverse | Explore Characters, Episodes & Locations",
-        description: "Discover characters, episodes, and locations from Rick and Morty. Dive into the multiverse and explore detailed information about your favorite characters.",
-        robots: "index, follow",
-        openGraph: {
-            title: "Rick and Morty Multiverse",
-            description: "Explore characters, episodes, and locations from Rick and Morty.",
-            url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
-            siteName: "Rick and Morty Multiverse",
-            images: [
-                {
-                    url: `${process.env.NEXT_PUBLIC_BASE_URL}/hero.webp`,
-                    width: 1280,
-                    height: 720,
-                    alt: "Rick and Morty Multiverse",
-                },
-            ],
-            locale: "en_US",
-            type: "website",
-        },
-        twitter: {
-            card: "summary_large_image",
-            title: "Rick and Morty Multiverse",
-            description: "Explore characters, episodes, and locations from Rick and Morty.",
-            images: [`${process.env.NEXT_PUBLIC_BASE_URL}/hero.webp`],
-        },
-    };
-}
-export const metadata = metadataObj;
-
 export default async function Page() {
     return (
         <main className="homecontent">
